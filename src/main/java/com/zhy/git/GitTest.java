07e3fc5fdafaba8db23d638f98456c103e13f1e8
zhy <1736045221@qq.com>
2023-05-28 10:46:40 +0800

    hot一次提交

--- src/main/java/com/zhy/git/GitTest.java
+++ src/main/java/com/zhy/git/GitTest.java
@@ -4,6 +4,6 @@ public class GitTest {
     public static void main(String[] args){
         System.out.println("hello");
         System.out.println("GIT");
-        System.out.println();
+        System.out.println("git");
     }
 }
